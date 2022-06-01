3a5491a7104e45e5a599e67bb160a957ce3e813b
marinadedev <marinadegames@gmail.com>
2022-06-01 17:58:23 +0300

    * added new database connection * added pg * added pg-pool

--- index.js
+++ index.js
@@ -2,34 +2,71 @@ import express from "express";
 import path, {dirname} from "path";
 import cool from 'cool-ascii-faces'
 import {fileURLToPath} from 'url';
+import dotenv from 'dotenv'
+import Pool from "pg-pool";
 
+dotenv.config()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const PORT = process.env.PORT || 5000;
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+console.log(process.env.DATABASE_URL)
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
+
     .get('/', (req, res) => {
         res.write(`<h1>HOME PAGE</h1>`)
         res.end()
     })
     .get('/tasks', (req, res) => {
-        res.write('TASKS')
+        res.send('TASKS')
         res.end()
     })
     .get('/cool', (req, res) => {
         res.send(cool())
         res.end()
     })
+    .get('/times', (req, res) => {
+        res.send(showTimes())
+        res.end()
+    })
+    .get('/db', async (req, res) => {
+        try {
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            const results = {'results': (result) ? result.rows : null};
+            res.render('pages/db', results);
+            client.release();
+        } catch (err) {
+            console.error(err);
+            res.send("Error " + err);
+        }
+    })
     .get('/*', (req, res) => {
         res.send(`<h1>404 not found</h1>`)
         res.end()
     })
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+    .listen(PORT, () => {
+        console.log(`Listening on ${PORT}`)
+    })
 
-
+const showTimes = () => {
+    let result = '';
+    const times = process.env.TIMES || 5;
+    for (let i = 0; i < times; i++) {
+        result += i + ' ';
+    }
+    return result;
+}
 // example (old code): todo: need delete in future
 
 // import chalk from "chalk";
