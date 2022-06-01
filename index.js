ec964bda19100e6c9099bc50219d82ebb68ee515
marinadedev <marinadegames@gmail.com>
2022-06-01 18:59:13 +0300

    * added new database connection * added pg * added pg-pool * create db table (test_table)

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ express()
             const client = await pool.connect();
             const result = await client.query('SELECT * FROM test_table');
             const results = {'results': (result) ? result.rows : null};
-            // res.render('pages/db', results);
             res.send(results)
             client.release();
         } catch (err) {
